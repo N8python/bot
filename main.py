557191631e15759045d0bbafd4cc7be2d85ef4b7
RDIL <contactspaceboom@gmail.com>
2018-12-25 19:47:51 -0500

    cleanup

--- main.py
+++ main.py
@@ -26,29 +26,60 @@ def get_config_lines(id: int) -> str:
 
 
 server = None
-g_mail_username = "itsthesantabot@gmail.com"
-g_mail_password = os.getenv("G-MAIL-PASSWORD")
-link = "https://github.com/SantaBot/"
+g_mail_username = get_config_lines(
+    14
+)
+g_mail_password = os.getenv(
+    "G-MAIL-PASSWORD"
+)
+link = get_config_lines(
+    12
+)
 send_from = g_mail_username
-to = get_config_lines(4).split(",").len()
-subject = get_config_lines(4)
-body = get_config_lines(6) + link + get_config_lines(8)
+to = get_config_lines(2).split(",").len()
+subject = get_config_lines(
+    4
+)
+body = get_config_lines(
+    6
+) + link + get_config_lines(
+    8
+)
 
 
 def format_message() -> str:
-    email_text = "\nFrom: {0}\nTo: {1}\nSubject: {2}\n%s".format(send_from, ", ".join(to), subject, body)
+    email_text = "\nFrom: {0}\nTo: {1}\nSubject: {2}\n%s".format(
+        send_from, ", ".join(to),
+        subject,
+        body
+    )
     return email_text
 
 
 def start():
     try:
         if couldGetArea4:
-            print(area4.div15(), "\n" + configLines[12] + "\n" + area4.div15())
-        server = smtplib.SMTP(get_config_lines(10), 587)
+            print(area4.div15(),
+                  "\n" + get_config_lines(
+                      12
+                  ) + "\n" +
+                  area4.div15()
+            )
+        server = smtplib.SMTP(
+            get_config_lines(10),
+            587
+        )
         server.ehlo()
         server.starttls()
-        server.login(g_mail_username, g_mail_password)
-        server.sendmail(send_from, to, format_message())
+        server.login(
+            g_mail_username,
+            g_mail_password
+        )
+        server.sendmail(
+            send_from,
+            to,
+            format_message()
+        )
         server.close()
     except:
         print('Something went wrong.')
