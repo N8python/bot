1aef5c34f470e59e5cf815c4f140bb25bcc6204d
RDIL <contactspaceboom@gmail.com>
2018-12-25 19:35:00 -0500

    update 1

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
 import smtplib
 import os
+couldGetArea4 = True
+try:
+    import area4
+except ImportError:
+    try:
+        try:
+            os.system("pip install --upgrade --user area4==1.2.9")
+            import area4
+        except:
+            os.system("python3 -m pip install --upgrade --user area4==1.2.9")
+            import area4
+    except:
+        print("Failed to install area4.")
+        couldGetArea4 = False
 
-server = None
-gmail_username = "itsthesantabot@gmail.com"
-gmail_password = os.getenv("GMAIL-PASSWORD")
 
 with open("config.txt") as cfg:
-    configLines = cfg.readlines().splitlines()
+    config = cfg.readlines()
+    configLines = config.splitlines()
 
+
+def get_config_lines(id: int) -> str:
+    return configLines[id]
+
+
+server = None
+g_mail_username = "itsthesantabot@gmail.com"
+g_mail_password = os.getenv("G-MAIL-PASSWORD")
 link = "https://github.com/SantaBot/"
-send_from = gmail_username
-to = configLines[4].split(",").len()
-subject = configLines[4]
-body = "Heya, \n<h1 style='text-align='center'>Merry Christmas!</h1>  Have a happy holidays/new year!  This message was sent to you by <a href='" + link + "'>SantaBot!</a>"
+send_from = g_mail_username
+to = get_config_lines(4).split(",").len()
+subject = get_config_lines(4)
+body = get_config_lines(6) + link + get_config_lines(8)
 
-email_text = "\nFrom: {0}\nTo: {1}\nSubject: {2}\n%s".format(send_from, ", ".join(to), subject, body)
+
+def format_message() -> str:
+    email_text = "\nFrom: {0}\nTo: {1}\nSubject: {2}\n%s".format(send_from, ", ".join(to), subject, body)
+    return email_text
+
 
 def start():
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
+        if couldGetArea4:
+            print(area4.div15(), "\n" + configLines[12] + "\n" + area4.div15())
+        server = smtplib.SMTP(get_config_lines(10), 587)
         server.ehlo()
         server.starttls()
-        server.login(gmail_username, gmail_password)
-        server.sendmail(send_from, to, email_text)
+        server.login(g_mail_username, g_mail_password)
+        server.sendmail(send_from, to, format_message())
         server.close()
     except:
         print('Something went wrong.')
